5de7e3b81e75c2cf9a0bb0baa54b85156f0cc88f
ayamahmod <ayamahmod1193@gmail.com>
2018-11-24 15:33:50 +0200

    Added get ahead problems 5&6 solns

--- Get Ahead/prob-5.cc
+++ Get Ahead/prob-5.cc
@@ -1,4 +1,5 @@
-// Given a 4X4 grid of letters and a dictionary, find the longest word from the dictionary that can be formed in the grid.
+// Given a 4X4 grid of letters and a dictionary, find the longest word from the
+// dictionary that can be formed in the grid.
 // The rules for forming a word are:
 // Start at any position on the board
 // Move in any of the up to 8 directions to choose another letter
@@ -19,3 +20,32 @@
 // E F G H
 // I J K L
 // M N O P
+#define GRID_SIZE 4
+
+void DFS(const vector<vector<char> >& grid, int i, int j,
+  vector<vector<bool> >& visited, string currWord, string& longestWord) {
+  if(visited[i][j]) return;
+  visited[i][j] = true;
+  currWord += grid[i][j];
+  if(isWord(currWord) && longestWord.size() < currWord.size()) longestWord = currWord; 
+  if(isPrefix(currWord)) {
+    int di[] = [-1, -1, -1, 0, 0, 1, 1, 1]
+    int dj[] = [-1, 0, 1, -1, 1, -1, 0, 1]
+    for(int k = 0; k < 8; ++k){
+      int idi = i+di[k], jdj = j+dj[k];
+      if(idi >= 0 && idi < GRID_SIZE && jdj >= 0 && jdj < GRID_SIZE)
+        DFS(grid, idi, jdj, visited, currWord, longestWord);
+    }
+  }
+}
+
+string longestWord(const vector<vector<char> >& grid) {
+  int size = GRID_SIZE;
+  string longestWord = "";
+  for(int i = 0; i < size; ++i) {
+    for(int j = 0; j < size; ++j) {
+      vector<vector<bool> > visited(size, vector<bool>(size, false));
+      DFS(grid, i, j, visited, "", longestWord);
+    }
+  }
+}
--- Get Ahead/prob-6.cc
+++ Get Ahead/prob-6.cc
@@ -1,4 +1,4 @@
-// We look at car licence plates and try to find a word from the dictionary
+// We look at car license plates and try to find a word from the dictionary
 // that includes all the letters from the licence plate.
 // The shorter the word, the better. The licence plates start with
 // two or three letters, then they are followed by 5 characters,
@@ -8,20 +8,41 @@
 // hortest word would be "SORT", for "RC 10014": "CAR".
 //
 // A few things to clarify:
-//
 // - Keep duplicate letters
-//
 // - Ordering is irrelevant
-//
 // - Case is irrelevant
-//
 // - The dictionary is sorted lexicographically
-//
 // - The dictionary contains about 4 million entries
 //
 // Test Cases
-//
 // For the licence plate "RT 123SO" the shortest word would be "SORT"
-//
 // For "RC 10014", the shortest word is "CAR".
-//
+
+bool comp(const string& s1, const string& s2) {
+  return s1.size() < s2.size();
+}
+
+string allLettersWord(const string& license, vector<string>& dict) {
+  string chars = "";
+  for(int i = 0; i < license.size(); ++i) {
+    if(license[i] >= "A" && license[i] <= "Z")
+      chars += license[i]+32;
+  }
+  sort(chars.begin(), chars.end());
+  sort(dict.begin(), dict.end(), comp);
+  for(int i = 0; i < dict.size(); ++i) {
+    if(dict[i].size() < chars.size()) continue;
+    sortedWord = dict[i];
+    sort(sortedWord.begin(), sortedWord.end());
+    int j = 0, k = 0;
+    do {
+      if(sortedWord[j] > chars[k]) break;
+      if(sortedDict[j] == chars[k]) ++k;
+      ++j;
+    } while(j < sortedWord.size() && k < chars.size());
+    if(k == chars.size()) return dict[i];
+  }
+}
+
+// Time complexity: O(nlogn)
+// Space complexity: O(n) sort in place or not?
